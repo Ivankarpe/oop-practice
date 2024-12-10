7a1b81e8d739c7241ef679dd5efcc1ee2ac03066
Ivankarpe <aakarpenko2013@gmail.com>
2024-12-10 13:43:22 +0200

    migration start

--- src/main/java/com/Karpenko/oop/Tour.java
+++ src/main/java/com/Karpenko/oop/Tour.java
@@ -1,26 +1,43 @@
 package com.Karpenko.oop;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tours")
 public class Tour {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
-    private int number;
+    private int count;
     private String date;
     private String tourGuy;
     private String place;
 
+    public void setId(long id) {
+        this.id = id;
+    }
     public void setDate(String date) {
         this.date = date;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public void setNumber(int number) {
-        this.number = number;
+    public void setNumber(int count) {
+        this.count = count;
     }
     public void setPlace(String place) {
         this.place = place;
     }
     public void setTourGuy(String tourGuy) {
         this.tourGuy = tourGuy;
+    }
+    public long getId() {
+        return id;
     }
     public String getDate() {
         return date;
@@ -29,7 +46,7 @@ public class Tour {
         return name;
     }
     public int getNumber() {
-        return number;
+        return count;
     }
     public String getPlace() {
         return place;
